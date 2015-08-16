6cbcc1c998f6cc4dba41f68442f16560ffa4d8c0
My Mac <mymac@GEusers-MacBook-Pro.local>
2015-08-16 20:20:58 +0530

    Initial Commit

---
+++ SingleViewSample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SingleViewSample
+//
+//  Created by My Mac on 16/08/15.
+//  Copyright (c) 2015 My Mac. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
